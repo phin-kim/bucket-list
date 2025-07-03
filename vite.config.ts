1909d31a91db0ae6a18046898f3ae3e11b08ece4
PHIN KIM <phinehasnjuguna1@gmail.com>
2025-07-03 22:37:58 +0300

    icon setup

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         name: 'Bucket List',
         short_name: 'BucketList',
         description: 'Track your goals & dreams!',
-        theme_color: '#ff6600',
+        theme_color: '#580558',
         background_color: '#ffffff',
         display: 'standalone',
         start_url: '/',
