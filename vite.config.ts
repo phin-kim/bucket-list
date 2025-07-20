c2c956fe44b7dbe96df195c9122ba71d62b504cb
PHIN KIM <phinehasnjuguna1@gmail.com>
2025-07-20 18:15:05 +0300

    final

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'pwa-icon-192.png', 'pwa-icon-512.png'],
